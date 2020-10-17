65d80c4b4fce03d4ce10f09601a7778fc0af6f84
AimeTPGM <aim.ske09@gmail.com>
2020-10-17 16:23:17 +0700

    Updates

---
+++ precache-manifest.33815e105cc1139748481c353ac33719.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb91e7120ecd36a80601d0647ccba55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f2599eea1340da8a70c",
+    "url": "/static/js/2.9c665241.chunk.js"
+  },
+  {
+    "revision": "ecd8629d666646c91b0a4a937ce8b37d",
+    "url": "/static/js/2.9c665241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "070e4a5537096e4f3ac0",
+    "url": "/static/js/main.7c2bc5da.chunk.js"
+  },
+  {
+    "revision": "bc0d6067cac55587bb47",
+    "url": "/static/js/runtime-main.77493c9c.js"
+  }
+]);
