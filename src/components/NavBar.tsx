364e9b8623d0ba093e678de0f929ae08892f931f
AimeTPGM <aim.ske09@gmail.com>
2020-10-17 20:23:48 +0700

    add logi

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,30 +2,49 @@
 import { jsx, css } from '@emotion/core';
 import React from 'react';
 import logo from '../assets/logo.jpg';
+import githubLogoWhite from '../assets/github-logo-white.png';
 
 export default () => {
     return (
-    <div css={{
-        background: '#403949',
-        color: '#FFF',
-        padding: '15px',
-        display: 'flex', 
-        justifyContent: 'center',
-        alignItems: 'center'
-    }}>
-        <div>
-            <img src={logo} alt="หมุดคณะราษฎร์ 2563 ไงว้อย" css={{ width: '100px'}} />
-        </div>
+        <div css={{background: '#403949',
+        color: '#FFF',}}>
 
-        <div css={{ padding: '0 10px', fontSize: '1.5em', fontWeight: 'bold', textTransform: 'uppercase'}}>
-            <div css={{ marginBottom: '10px'}}>
-                <span>เพื่ออนาคตที่ดีกว่า</span>
+            <div css={{
+                    
+                    padding: '15px',
+                    display: 'flex', 
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                <div>
+                    <img src={logo} alt="หมุดคณะราษฎร์ 2563 ไงว้อย" css={{ width: '100px'}} />
+                </div>
+
+                <div css={{ padding: '0 10px', fontSize: '1.5em', fontWeight: 'bold', textTransform: 'uppercase'}}>
+                    <div css={{ marginBottom: '10px'}}>
+                        <span>เพื่ออนาคตที่ดีกว่า</span>
+                    </div>
+
+                    <div>
+                        <span>For the better future</span>
+                    </div>
+                    
+                </div>
+                
             </div>
 
-            <div>
-                <span>For the better future</span>
+
+        <div css={{ display: 'flex', alignItems: 'center', padding: '20px',justifyContent: 'center'}}>
+            <div css={{ margin: '0 10px'}}>
+                <a href="https://github.com/AimeTPGM/mobfeeds">
+                    <img src={githubLogoWhite} alt="ช่วยหน่อยนะ" css={{ width: '25px'}}  />
+                </a>
             </div>
-            
+            <div css={{ textAlign: 'left', fontSize: '10px'}}>
+            you know how to code?
+            <br />
+            We need your help!
+            </div>
         </div>
         
     </div>
