02855b1b9dd3a09ac0830ae900155145248538bb
AimeTPGM <aim.ske09@gmail.com>
2020-10-17 16:08:02 +0700

    add files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        test
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+
+const AppWithRouter = () => (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+)
+  
+ReactDOM.render(<AppWithRouter />, document.getElementById('root'))
+serviceWorker.unregister();
