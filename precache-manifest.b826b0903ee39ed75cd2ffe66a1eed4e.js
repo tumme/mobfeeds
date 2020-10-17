8fb4177d5b819d2aa15879db07708cf0649c15fb
AimeTPGM <aim.ske09@gmail.com>
2020-10-17 17:25:20 +0700

    Updates

---
+++ precache-manifest.b826b0903ee39ed75cd2ffe66a1eed4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce14f8d162173a20d1fbe1a721ccf71a",
+    "url": "/mobfeeds/index.html"
+  },
+  {
+    "revision": "8426bff6640e8177d9ab",
+    "url": "/mobfeeds/static/css/main.cff725f2.chunk.css"
+  },
+  {
+    "revision": "cb24d73a755b8b226e0e",
+    "url": "/mobfeeds/static/js/2.ed50f3fd.chunk.js"
+  },
+  {
+    "revision": "ecd8629d666646c91b0a4a937ce8b37d",
+    "url": "/mobfeeds/static/js/2.ed50f3fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8426bff6640e8177d9ab",
+    "url": "/mobfeeds/static/js/main.c0e2aae0.chunk.js"
+  },
+  {
+    "revision": "4f3bd508205206a491f0",
+    "url": "/mobfeeds/static/js/runtime-main.3ca6cae6.js"
+  },
+  {
+    "revision": "06027ab7e34e2f4e8682f5ec4fc44562",
+    "url": "/mobfeeds/static/media/bangna-new.06027ab7.ttf"
+  },
+  {
+    "revision": "4245d47887a5d9dd791177a86f374075",
+    "url": "/mobfeeds/static/media/logo.4245d478.jpg"
+  }
+]);
